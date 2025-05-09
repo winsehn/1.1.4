3b2b28af2c372091bd16fe358a33458a3a126bd0
winsehn <winsehn@yandex.ru>
2025-05-09 19:36:52 +0300

    For review

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,98 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private String createTable = """
+            CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            lastName VARCHAR(255) NOT NULL,
+            age INT NOT NULL)
+            """;
+    private String dropUsersTable = "DROP TABLE IF EXISTS users";
+    private String saveUser = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+    private String removeUserById = "DELETE FROM users WHERE id = ?";
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (Connection conn = Util.getConnection();
+             Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(createTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection conn = Util.getConnection();
+             Statement stm = conn.createStatement()) {
+            stm.executeUpdate(dropUsersTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection conn = Util.getConnection();
+             PreparedStatement pstm = conn.prepareStatement(saveUser)) {
+            pstm.setString(1, name);
+            pstm.setString(2, lastName);
+            pstm.setInt(3, age);
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection conn = Util.getConnection();
+             PreparedStatement pstm = conn.prepareStatement(removeUserById)) {
+            pstm.setLong(1, id);
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<User>();
+        try (Connection conn = Util.getConnection();
+             Statement stm = conn.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT * FROM users")) {
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setLastName(rs.getString("lastName"));
+                user.setAge(rs.getByte("age"));
+                users.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String sqlClean = "DELETE FROM users";
+        try (Connection conn = Util.getConnection();
+             PreparedStatement pstm = conn.prepareStatement(sqlClean)) {
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String USER = "root";
+    private static final String PASSWORD = "testSQL123";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+
+    private Util() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
